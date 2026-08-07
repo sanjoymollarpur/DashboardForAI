2e92b43a120b5566b27f35e669b0304807a3a124
Sanjoy Mondal <sanjoymlp2014@gmail.com>
2026-08-07 09:47:11 +0530

    user project add

--- pages/admin_dashboard.py
+++ pages/admin_dashboard.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import sqlite3
+from database.user_db import add_value
+import pandas as pd
 
 def admin_dashboard():
     st.title("Admin Dashboard")
+
+    file_path="bank_ai1.db"
+    conn = sqlite3.connect(file_path)
+    cursor = conn.cursor()
+    query=f"SELECT * FROM user_table1"
+    cursor.execute(query)
+    
+    rows = cursor.fetchall()
+    conn.close()
+    df = pd.DataFrame(
+    rows,
+    columns=["id", "user_name", "project_name", "project_description", "project_state", "department", "problem"]
+    )
+    st.dataframe(df, width="stretch")
+
+    
--- pages/user_dashboard.py
+++ pages/user_dashboard.py
@@ -15,24 +15,42 @@ def user_dashboard(user):
     cursor.execute(query)
     
     rows = cursor.fetchall()
+    conn.close()
     df = pd.DataFrame(
     rows,
     columns=["id", "user_name", "project_name", "project_description", "project_state", "department", "problem"]
     )
     st.dataframe(df, width="stretch")
 
-    # col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
+
+    if "action" not in st.session_state:
+        st.session_state.action = None
     
     if "show_form" not in st.session_state:
         st.session_state.show_form = False
 
-    # Add button
-    if st.button("➕ Add Project"):
-        st.session_state.show_form = True
+    with col1:
+        if st.button("➕ Add", width="stretch"):
+            st.session_state.action = "add"
+            st.session_state.show_form = True
 
-    # Show form only after button click
-    if st.session_state.show_form:
+    with col2:
+        if st.button("✏️ Update", width="stretch"):
+            st.session_state.action = "update"
 
+    with col3:
+        if st.button("🗑 Delete", width="stretch"):
+            st.session_state.action = "delete"
+
+    if st.session_state.action == "update":
+        st.write("Update form")
+
+    elif st.session_state.action == "delete":
+        st.write("Delete form")
+
+    elif st.session_state.action == "add" and st.session_state.show_form:
+        st.write("Add form")
         with st.form("project_form"):
             user_name = user
             project_name = st.text_input("Project Name")
@@ -55,6 +73,32 @@ def user_dashboard(user):
                 # Hide the form after saving (optional)
                 st.session_state.show_form = False
                 st.rerun()
+
+    
+    # if st.session_state.action == "update":
+    #     with st.form("update_form"):
+    #         record_id = st.number_input("Record ID", min_value=1)
+    #         project_name = st.text_input("Project Name")
+    #         project_description = st.text_area("Project Description")
+    #         project_state = st.selectbox(
+    #             "Project State",
+    #             ["POC", "Pilot", "Developed"]
+    #         )
+    #         department = st.text_input("Department")
+    #         problem = st.text_area("Problem")
+    #         submit = st.form_submit_button("Update")
+    #         if submit:
+    #             update_value(
+    #                 record_id,
+    #                 project_name,
+    #                 project_description,
+    #                 project_state,
+    #                 department,
+    #                 problem
+    #             )
+    #             st.success("Project updated successfully!")
+    #             st.session_state.action = None
+    #             st.rerun()
     
     # with col1:
     #     add = st.button("➕ Add", width="stretch")
